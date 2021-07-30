331d9ffa2fe5cf7fd2383cfb8fa44402368c9694
hungry44therock <hungry44therock@gmail.com>
2021-07-30 15:25:23 +0900

    all emergency

---
+++ Java-master/Book/src/ch01/P31.java
@@ -0,0 +1,11 @@
+package ch01;
+
+/*
+ * 날짜 :
+ * 이름 :
+ * 내용 : 'Hello Java!' 출력하기
+ * 
+ */
+public class P31 {
+
+}
---
+++ Java-master/Ch06/src/sub5/AbstractTest.java
@@ -0,0 +1,38 @@
+package sub5;
+
+/*
+ * 날짜 : 2021/05/25
+ * 이름 : 김철학
+ * 내용 : 추상클래스 실습하기
+ */
+public class AbstractTest {
+
+	public static void main(String[] args) {
+		
+		//Unit unit = new Unit();
+		Unit marin    = new Marin();
+		Unit zealot   = new Zealot();
+		Unit zergling = new Zergling();
+				
+		marin.move();
+		zealot.move();
+		zergling.move();
+		
+		marin.attack();
+		zealot.attack();
+		zergling.attack();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
